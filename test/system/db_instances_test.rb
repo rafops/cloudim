be8d349ebdbc54c5520229e8c9d9c86a4e35c261
Rafael Moraes <contact@moraesrafael.com>
2017-10-07 12:38:39 -0400

    minitest -> rspec

--- test/system/db_instances_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class DbInstancesTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit db_instances_url
-  #
-  #   assert_selector "h1", text: "DbInstance"
-  # end
-end
--- test/system/instances_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class InstancesTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit instances_url
-  #
-  #   assert_selector "h1", text: "Instance"
-  # end
-end
