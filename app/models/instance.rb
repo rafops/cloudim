1c81552fa958e8ebe3e90b9575213fd025e0a20b
Steph <stephaniem@nulogy.com>
2017-10-13 14:34:33 -0400

    Adds an instance view and link from index

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -3,7 +3,7 @@ class Instance < ApplicationRecord
   versionable identifier_column: "instance_id", version_column: "valid_until"
 
   belongs_to :account
-
+  
   class << self
     def head_transaction(account, region, &block)
       transaction do
